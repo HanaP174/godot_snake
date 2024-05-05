81387f75c75fb22fca6ee88cef0e99ec479e0fa3
HanaP174 <61006773+HanaP174@users.noreply.github.com>
2024-05-05 18:46:55 +0200

    renaming

--- playground/Spawner.cs
+++ playground/Spawner.cs
@@ -4,7 +4,7 @@ using System;
 public partial class Spawner : Node2D
 {
 	[Export]
-	private Bounds bounds;
+	private Bounds _bounds;
 
 	private Random _random;
 	private PackedScene _treatScene;
@@ -18,8 +18,8 @@ public partial class Spawner : Node2D
 	public void SpawnTreat()
 	{
 		Vector2 treatPosition = Vector2.Zero;
-		treatPosition.X = _random.Next((int) bounds.XMin + 80, (int) bounds.XMax - 80);
-		treatPosition.Y = _random.Next((int) bounds.YMin + 80, (int) bounds.YMax - 80);
+		treatPosition.X = _random.Next((int) _bounds.XMin + 80, (int) _bounds.XMax - 80);
+		treatPosition.Y = _random.Next((int) _bounds.YMin + 80, (int) _bounds.YMax - 80);
 		if (_treatScene.Instantiate() is Area2D treat)
 		{
 			treat.Position = treatPosition;
